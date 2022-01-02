b029de89f57cbf9cc049eebc4579ae9080b9391e
Giona Paolini <gionapaolini@gmail.com>
2022-01-02 21:48:30 +0100

    finalize version 1

--- main.go
+++ main.go
@@ -6,44 +6,39 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
+
+	programPath := getProgramPath()
+	os.Chdir(programPath)
+
+	pgid := startDebugging()
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
-	defer watcher.Close()
-
-	cmd := exec.Command("tail", "-f", os.Args[1]+"/hello1")
-
-	stdout, _ := cmd.StdoutPipe()
-	cmd.Start()
-
-	scanner := bufio.NewScanner(stdout)
-	scanner.Split(bufio.ScanWords)
-	for scanner.Scan() {
-		m := scanner.Text()
-		fmt.Println(m)
-	}
-	cmd.Wait()
-
-	cmd.Process.Signal(os.Kill)
-
 	done := make(chan bool)
 	go func() {
 		for {
 			select {
-			case event, ok := <-watcher.Events:
+			case evnt, ok := <-watcher.Events:
 				if !ok {
 					return
 				}
-				log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
+				if !strings.HasSuffix(evnt.Name, ".go") {
+					continue
 				}
+				println("restarting..")
+				syscall.Kill(-pgid, 15)
+				pgid = startDebugging()
+				println("..restarted")
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
@@ -52,10 +47,44 @@ func main() {
 			}
 		}
 	}()
-
-	err = watcher.Add(os.Args[1])
+	err = watcher.Add(programPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	<-done
+
+}
+
+func startDebugging() int {
+	cmd := exec.Command("dlv", "debug", "--headless", "--listen=:2345", "--api-version=2", "--accept-multiclient")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid:   true,
+		Pdeathsig: syscall.SIGTERM,
+	}
+
+	stdout, _ := cmd.StdoutPipe()
+	cmd.Start()
+
+	scanner := bufio.NewScanner(stdout)
+	scanner.Split(bufio.ScanWords)
+	go func() {
+		for scanner.Scan() {
+			m := scanner.Text()
+			fmt.Println(m)
+		}
+	}()
+	pgid, err := syscall.Getpgid(cmd.Process.Pid)
+	if err != nil {
+		panic(err)
+	}
+	return pgid
+}
+
+func getProgramPath() string {
+	mydir, err := os.Getwd()
+	if err != nil {
+		log.Panic(err)
+	}
+	programPath := filepath.Join(mydir, os.Args[1])
+	return programPath
 }
